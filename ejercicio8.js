bf28ff9668cce0f93e853ee023fadcec98446e91
Jenni Arabel <jennifer.arabel@gmail.com>
2025-07-21 20:03:59 -0300

    Pasar de if - else a switch -- usar toLowerCase para que solo las vocales sean transformadas a minúsculas.

--- ejercicio8.js
+++ ejercicio8.js
@@ -6,18 +6,47 @@ function replazarVocalPorNumero(cadena) {
     let letra = cadena[i];
 
     // Reemplaza las vocales por su correspondiente número
-    if (letra === "A" || letra === "a") {
-      resultado += "4";
-    } else if (letra === "E" || letra === "e") {
-      resultado += "3";
-    } else if (letra === "I" || letra === "i") {
-      resultado += "1";
-    } else if (letra === "O" || letra === "o") {
-      resultado += "0";
-    } else if (letra === "U" || letra === "u") {
-      resultado += "2";
-    } else {
-      resultado += letra;
+
+    /* if (letra === "A" || letra === "a") {
+       resultado += "4";
+     } else if (letra === "E" || letra === "e") {
+       resultado += "3";
+     } else if (letra === "I" || letra === "i") {
+       resultado += "1";
+     } else if (letra === "O" || letra === "o") {
+       resultado += "0";
+     } else if (letra === "U" || letra === "u") {
+       resultado += "2";
+     } else {
+       resultado += letra;
+     }
+      */
+
+    // Pasar a switch - max 3 if else
+    
+    // Convertimos la letra a minúscula SOLO para la comparación en el switch
+    // Esto permite que el switch maneje 'A' y 'a' de la misma forma,
+    // pero si la letra no es una vocal, se agregará al resultado en su case original.
+
+    switch (letra.toLowerCase()) {
+      case "a":
+        resultado += "4";
+        break;
+      case "e" :
+        resultado += "3";
+        break;
+      case "i":
+        resultado += "1";
+        break;
+      case "o":
+        resultado += "0";
+        break;
+      case "u":
+        resultado += "2";
+        break;
+      default:
+        resultado += letra; // Si no es vocal, se agrega la letra original
+        break;
     }
   }
   return resultado;
